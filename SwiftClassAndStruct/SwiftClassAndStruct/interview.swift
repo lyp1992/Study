97d26292c667e3e0fbbc39c639abfc304c70f941
lyp1992 <2294582526@qq.com>
2019-11-17 23:59:27 +0800

    增加面试题

--- SwiftClassAndStruct/SwiftClassAndStruct/interview.swift
+++ SwiftClassAndStruct/SwiftClassAndStruct/interview.swift
@@ -34,3 +34,28 @@ func testInterView()  {
     p.x = 20;
     p.y = 30;
 }
+
+// 2. 下面size类和point结构体的内存结构是怎么样的？ size和point分别占用多少个字节
+class Size {
+    var width : Int = 1
+    var height : Int = 2
+}
+
+struct Point {
+    var x : Int = 3
+    var y : Int = 4
+   
+}
+// 3. class 的创建过程是什么？
+
+// 4. 枚举的内存结构是什么样的？ 原始值和关联值有什么区别？
+enum Season {
+    case spring,summer,autumn,winter
+}
+
+enum Password {
+    case number (Int, Int, Int, Int)
+    case other
+}
+
+// 5. 手写一个方法前缀，比如：xx.BD.instert.....
--- SwiftClassAndStruct/SwiftClassAndStruct/main.swift
+++ SwiftClassAndStruct/SwiftClassAndStruct/main.swift
@@ -33,34 +33,57 @@ import Foundation
 
 
 // 本质区别
-func test(){
-    
-    class Size {
-        var width : Int = 1
-        var height : Int = 2
-    }
+//func test(){
+//
+//    class Size {
+//        var width : Int = 1
+//        var height : Int = 2
+//    }
+//
+//    struct Point {
+//        var x : Int = 3
+//        var y : Int = 4
+//    }
+//
+//    var size = Size()
+//
+////    print("size变量地址", Mems.ptr(ofVal: &size))
+////    print("size变量的内存",Mems.memStr(ofVal: &size))
+//
+//    print("size所指向内存的地址",Mems.ptr(ofRef: size))
+//    print("size所指向内存的内容",Mems.memStr(ofRef: size))
+//
+//    var point = Point()
+//    print("point变量地址", Mems.ptr(ofVal: &point))
+//    print("point变量的内存",Mems.memStr(ofVal: &point))
+//
+//    print("point所指向内存的地址",Mems.ptr(ofRef: point))
+//    print("point所指向内存的内容",Mems.memStr(ofRef: point))
+//
+//}
+//test()
 
-    struct Point {
-        var x : Int = 3
-        var y : Int = 4
-    }
-    
-    var size = Size()
-    
-//    print("size变量地址", Mems.ptr(ofVal: &size))
-//    print("size变量的内存",Mems.memStr(ofVal: &size))
-
-    print("size所指向内存的地址",Mems.ptr(ofRef: size))
-    print("size所指向内存的内容",Mems.memStr(ofRef: size))
-    
-    var point = Point()
-    print("point变量地址", Mems.ptr(ofVal: &point))
-    print("point变量的内存",Mems.memStr(ofVal: &point))
-
-    print("point所指向内存的地址",Mems.ptr(ofRef: point))
-    print("point所指向内存的内容",Mems.memStr(ofRef: point))
-
+enum Password {
+    case number (Int, Int, Int)
+    case string(String)
+    case other
 }
-
+  
+func test() {
+    
+    var pwd = Password.number(1, 2, 3)
+//    print("pwd = ", Mems.ptr(ofVal: &pwd))
+    pwd = Password.string("hello world")
+//    pwd = Password.other
+    
+    switch pwd {
+    case let .number(n1, n2, n3):
+        print("num is ,", n1, n2, n3)
+    case let .string(str):
+        print("is string", str)
+    default:
+        print("is other")
+    }
+}
 
 test()
